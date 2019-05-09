5bbb2922dc639a1453897f0299dc93047dd3c3e5
manuel mazzatelli <manuel.narut@gmail.com>
2019-05-09 11:00:04 +0200

    js basic

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,5 @@
 import _ from 'lodash';
 
-function component() {
-  const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack2'], ' ');
 
-  return element;
-}
 
-document.body.appendChild(component());
