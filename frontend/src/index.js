66e25831f272a06492cbad3ba0989c8cfddac60a
manuel mazzatelli <manuel.narut@gmail.com>
2019-05-17 12:08:31 +0200

    login parziale js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,18 @@
 import _ from 'lodash';
+import io from 'socket.io-client';
 
-document.getElementById('title').innerText = "Hello world!!!";
+const socket = io('http://localhost');
+
+socket.on('login', function(msg){
+    // un utente si è connesso
+ let nome_utente = msg.nickName
+ let nome_personaggio= msg.character.name
+});
+$( "#buttonerators" ).click(function() {
+    let nickName =$( "#single" ).val();
+  });
+/*
+on form submit function() {
+    socket.emit('login', { nickName: valore dell'input })
+}
+*/
